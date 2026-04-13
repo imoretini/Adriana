d6a1f70b7682934bc593abadb759e6357c825625
Ieda Moretini <imoretini@hotmail.com>
2026-04-13 13:25:22 -0300

    Atualizações em Farmacia_2. Add interface grafica

---
+++ Exercicios_LP1/Farmcia_2/src/main/java/org/example/RemedioController.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class RemedioController {
+
+    @FXML
+    private Button btVerificaEficacia;
+
+    @FXML
+    private TextField txtDoseIngerida;
+
+    @FXML
+    private TextField txtDoseReceita;
+
+    @FXML
+    private TextField txtNomeRemedio;
+
+}
